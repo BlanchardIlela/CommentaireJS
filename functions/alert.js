d697d5fa34892b92f55c9930d0db32b64a5ec36e
BlanchardIlela <blancilela@gmail.com>
2025-10-01 07:19:21 +0100

    modification de type d'alert

--- functions/alert.js
+++ functions/alert.js
@@ -2,8 +2,9 @@
  * Renvoie un élément HTML représentant une alerte 
  * @param {string} message 
  */
-export function alertElement(message) {
+export function alertElement(message, type = 'danger') {
     const el = document.querySelector('#alert').content.firstElementChild.cloneNode(true)
+    el.classList.add(`alert-${type}`)
     el.querySelector('.js-text').innerText = message
     el.querySelector('button').addEventListener('click', e => {
         e.preventDefault()
